fa0a65f7b506c176e89a69c631f687467ea806cf
MJK <muhammadkhusravi@gmail.com>
2024-07-14 14:54:42 -0700

    Random number added to email

--- src/test/java/tek/review/week1/Activity1.java
+++ src/test/java/tek/review/week1/Activity1.java
@@ -1,5 +1,6 @@
 package tek.review.week1;
 
+import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,15 +11,22 @@ public class Activity1 {
         driver.get("https://retail.tekschool-students.com/");
         Thread.sleep(3000);
         driver.get("https://retail.tekschool-students.com/");
+
+        String emailPrefix = "khusravi_m";
+        int number = (int) (Math.random() * 1000);
+        String email = emailPrefix + number + "@gmail.com";
+
         driver.findElement(By.id("signinLink")).click();
         driver.findElement(By.id("newAccountBtn")).click();
         driver.findElement(By.id("nameInput")).sendKeys("Khusravi");
-        driver.findElement(By.id("emailInput")).sendKeys("KhusraviM@gmail.com");
+        driver.findElement(By.id("emailInput")).sendKeys(email);
         driver.findElement(By.id("passwordInput")).sendKeys("Pass@123");
         driver.findElement(By.id("confirmPasswordInput")).sendKeys("Pass@123");
         driver.findElement(By.id("signupBtn")).click();
         Thread.sleep(1000);
         System.out.println(driver.findElement(By.id("profileImage")).isDisplayed());
+//        boolean isProfilePicDisplayed = driver.findElement(By.id("profileImage")).isDisplayed();
+//        Assert.isTrue(isProfilePicDisplayed, "Profile image is not displayed");
 
         Thread.sleep(5000);
         driver.quit();
---
+++ src/test/java/tek/review/week1/RandomNumber.java
@@ -0,0 +1,15 @@
+package tek.review.week1;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        double random = Math.random();
+        //random = random *10000;
+        //System.out.println(random);
+
+//        int number = (int)(random * 10);
+//        System.out.println(number);
+
+
+
+    }
+}
