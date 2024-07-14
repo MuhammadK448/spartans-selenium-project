bfd188ef27aece67487a18d9fde0413cda1ad94f
MJK <muhammadkhusravi@gmail.com>
2024-07-14 00:25:18 -0700

    Activity 2 is Completed

--- src/test/java/tek/intro/Activity2.java
+++ src/test/java/tek/intro/Activity2.java
@@ -2,15 +2,14 @@ package tek.intro;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OpenFacebook {
+public class Activity2 {
     public static void main(String[] args) {
         ChromeDriver driver = new ChromeDriver();
+        driver.get("https://dev.insurance.tekschool-students.com/");
         driver.manage().window().maximize();
-        driver.get("https://facebook.com");
+        String title = driver.getTitle();
+        System.out.println(title);
 
-        System.out.println(driver.getTitle());
-
-        //Thread.Sleep(3000);
-        driver.close();
+        //driver.close();
     }
 }
